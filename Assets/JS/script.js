a31ff63d2243981826c35b7fe709e2401b3bbff1
MatheusJunior2334 <124627522+MatheusJunior2334@users.noreply.github.com>
2023-04-03 01:00:38 -0300

    Mapa Interativo Finalizado

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,74 @@
 //Início do Mapa Interativo
 
-document.querySelectorAll ('path')
-.forEach (elemento => {
-    elemento.addEventListener ('mouseover', () => {
-        elemento.setAttribute ('class', 'hover')
+//Parte 1 - Título do Mapa
+
+document.querySelectorAll('path')
+.forEach(elemento => {
+    elemento.addEventListener('mouseover', () => {
+        elemento.setAttribute('class', 'hover')
     })
 
-    elemento.addEventListener ('mouseout', () => {
-        elemento.setAttribute ('class', 'fill str0')
+    elemento.addEventListener('mouseout', () => {
+        elemento.setAttribute('class', 'fill str0')
     })
 
-    elemento.addEventListener ('click', () => {
-        document.querySelector ('#microregiao_nome').textContent = elemento.attributes.id.nodeValue
+    elemento.addEventListener('click', () => {
+        document.querySelector('#regiao_nome').textContent = elemento.attributes.id.nodeValue
     })
 })
+
+//Parte 2 - Texto da Região
+
+function regiao_leste() {
+    document.getElementById("regiao_leste").style.display="block";
+    document.getElementById("regiao_norte").style.display="none";
+    document.getElementById("regiao_oeste").style.display="none";
+    document.getElementById("regiao_sudeste").style.display="none";
+    document.getElementById("regiao_sudoeste").style.display="none";
+    document.getElementById("regiao_central").style.display="none";
+}
+
+function regiao_norte() {
+    document.getElementById("regiao_leste").style.display="none";
+    document.getElementById("regiao_norte").style.display="block";
+    document.getElementById("regiao_oeste").style.display="none";
+    document.getElementById("regiao_sudeste").style.display="none";
+    document.getElementById("regiao_sudoeste").style.display="none";
+    document.getElementById("regiao_central").style.display="none";
+}
+
+function regiao_oeste () {
+    document.getElementById("regiao_leste").style.display="none";
+    document.getElementById("regiao_norte").style.display="none";
+    document.getElementById("regiao_oeste").style.display="block";
+    document.getElementById("regiao_sudeste").style.display="none";
+    document.getElementById("regiao_sudoeste").style.display="none";
+    document.getElementById("regiao_central").style.display="none";
+}
+
+function regiao_sudeste () {
+    document.getElementById("regiao_leste").style.display="none";
+    document.getElementById("regiao_norte").style.display="none";
+    document.getElementById("regiao_oeste").style.display="none";
+    document.getElementById("regiao_sudeste").style.display="block";
+    document.getElementById("regiao_sudoeste").style.display="none";
+    document.getElementById("regiao_central").style.display="none";
+}
+
+function regiao_sudoeste () {
+    document.getElementById("regiao_leste").style.display="none";
+    document.getElementById("regiao_norte").style.display="none";
+    document.getElementById("regiao_oeste").style.display="none";
+    document.getElementById("regiao_sudeste").style.display="none";
+    document.getElementById("regiao_sudoeste").style.display="block";
+    document.getElementById("regiao_central").style.display="none";
+}
+
+function regiao_central () {
+    document.getElementById("regiao_leste").style.display="none";
+    document.getElementById("regiao_norte").style.display="none";
+    document.getElementById("regiao_oeste").style.display="none";
+    document.getElementById("regiao_sudeste").style.display="none";
+    document.getElementById("regiao_sudoeste").style.display="none";
+    document.getElementById("regiao_central").style.display="block";
+}
